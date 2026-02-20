a744a77835dae901d048dafbd4d079a2fc160487
= <=>
2026-02-20 08:55:13 -0300

    first commit

---
+++ src/main/java/com/scada/ScadaCR/ScadaCrApplication.java
@@ -0,0 +1,13 @@
+package com.scada.ScadaCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScadaCrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScadaCrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scada/ScadaCR/ScadaCrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scada.ScadaCR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScadaCrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
